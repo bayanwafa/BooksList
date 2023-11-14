24009f319ed528a7a168f60034186bcccaef2564
bayanwafa <bayan.wafa@gmail.com>
2023-11-14 14:23:19 -0800

    putted a simple heading and a paragraph

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>My Book List</h1>
+      <p>This is a simple React application to manage your book list.</p>
     </div>
   );
 }
